b4b9621ad9017d96ed9d3d8c89f75bc6c0284da5
Natalia Caldera <ncaldera@umich.edu>
2024-09-28 21:24:41 -0400

    Initial Commit

---
+++ Buraco fin/Buraco_finApp.swift
@@ -0,0 +1,17 @@
+//
+//  Buraco_finApp.swift
+//  Buraco fin
+//
+//  Created by Natalia Caldera on 9/28/24.
+//
+
+import SwiftUI
+
+@main
+struct Buraco_finApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Buraco finTests/Buraco_finTests.swift
@@ -0,0 +1,17 @@
+//
+//  Buraco_finTests.swift
+//  Buraco finTests
+//
+//  Created by Natalia Caldera on 9/28/24.
+//
+
+import Testing
+@testable import Buraco_fin
+
+struct Buraco_finTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
